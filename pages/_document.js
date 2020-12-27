3b8723aedbd4e70eb657a55d7dbd3260a35e78c2
aaronconway7 <aaronconway796@gmail.com>
2020-12-27 23:48:18 +0000

    SEO

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+import FacebookCover from '../public/facebook-cover-art.jpg'
+
 export default class MyDocument extends Document {
     render() {
         return (
@@ -21,11 +23,29 @@ export default class MyDocument extends Document {
                             `,
                         }}
                     />
+                    <meta charset='utf-8' />
+                    <meta name='viewport' content='width=device-width,initial-scale=1.0' />
+                    <title>The Third Wheel</title>
+                    <meta
+                        name='description'
+                        content={`🎙️ A podcast where we're joined by a “Third Wheel” each episode to learn a bit more about them, debate various topics and (occasionally) play some games whilst your hosts Aaron & Hamish take shots at each other and act as if they understand everything being spoken about 🙃. New episode every Sunday! 🚨`}
+                    />
+                    <link rel='canonical' href='https://thethirdwheel.fm' />
+                    <meta name='theme-color' content='#4adfb8' />
+                    <meta property='og:title' content='The Third Wheel' />
+                    <meta
+                        property='og:description'
+                        content={`🎙️ A podcast where we're joined by a “Third Wheel” each episode to learn a bit more about them, debate various topics and (occasionally) play some games whilst your hosts Aaron & Hamish take shots at each other and act as if they understand everything being spoken about 🙃. New episode every Sunday! 🚨`}
+                    />
+                    <meta property='og:image' content={FacebookCover} />
+                    <meta property='og:url' content='https://thethirdwheel.fm' />
+                    <meta name='twitter:card' content='summary_large_image' />
+                    <meta property='og:site_name' content='The Third Wheel' />
+                    <meta name='twitter:image:alt' content='The Third Wheel' />
                     <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
                     <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
                     <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
                     <link rel='manifest' href='/site.webmanifest' />
-                    <title>The Third Wheel</title>
                 </Head>
                 <body>
                     <Main />
